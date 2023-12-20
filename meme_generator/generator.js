c86bb4ec0a8b9415181b6db587837eb8646e0d0a
Brendon Karuru <brendonkaruru21@gmail.com>
2023-12-20 10:56:13 +0200

    meme-generator

---
+++ meme_generator/generator.js
@@ -0,0 +1,23 @@
+ const generateMemeBtn = document.querySelector(".meme_generator .generate-meme-btn");
+ const memeImage = document.querySelector(".meme_generator img");
+ const memeTitle = document.querySelector(".meme_generator .meme-title");
+ const memeAuthor = document.querySelector(".meme_generator .meme-author");
+
+const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+};
+
+ const generateMeme = () => {
+    fetch("https://meme-api.herokuapp.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then((data) => {
+            updateDetails(data.url, data.title, data.author);
+        });
+    
+ };
+
+ generateMemeBtn.addEventListener("click", generateMeme);
+
+ generateMeme();
