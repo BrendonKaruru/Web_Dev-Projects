ecd19bde4b41e6c0e129df31764db6e7c09fac12
Brendon Karuru <brendonkaruru21@gmail.com>
2023-12-20 11:17:44 +0200

    Meme-generator

--- meme_generator/generator.js
+++ meme_generator/generator.js
@@ -1,7 +1,9 @@
- const generateMemeBtn = document.querySelector(".meme_generator .generate-meme-btn");
- const memeImage = document.querySelector(".meme_generator img");
- const memeTitle = document.querySelector(".meme_generator .meme-title");
- const memeAuthor = document.querySelector(".meme_generator .meme-author");
+const generateMemeBtn = document.querySelector(
+    ".meme_generator .generator-meme-btn"
+);
+const memeImage = document.querySelector(".meme_generator img");
+const memeTitle = document.querySelector(".meme_generator .meme-title");
+const memeAuthor = document.querySelector(".meme_generator .meme-author");
 
 const updateDetails = (url, title, author) => {
     memeImage.setAttribute("src", url);
@@ -9,15 +11,15 @@ const updateDetails = (url, title, author) => {
     memeAuthor.innerHTML = `Meme by: ${author}`;
 };
 
- const generateMeme = () => {
+const generateMeme = () => {
     fetch("https://meme-api.herokuapp.com/gimme/wholesomememes")
     .then((response) => response.json())
     .then((data) => {
-            updateDetails(data.url, data.title, data.author);
-        });
+        updateDetails(data.url, data.title, data.author);
+    });
     
- };
+};
 
- generateMemeBtn.addEventListener("click", generateMeme);
+generateMemeBtn.addEventListener("click", generateMeme);
 
- generateMeme();
+generateMeme();
